1372f1c9f5f1800e2404deccf1f54c50297e239a
julianguerra1231186-crypto <julianguerra1231186@gmail.com>
2026-02-15 12:15:46 -0500

    feat: complete client creation flow witch validadation and persistence

--- src/main/java/com/julianguerra/financial_app/entity/Client.java
+++ src/main/java/com/julianguerra/financial_app/entity/Client.java
@@ -1,4 +1,48 @@
 package com.julianguerra.financial_app.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "clients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String identificationType;
+
+    @Column(unique = true, nullable = false)
+    private String identificationNumber;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    private LocalDate birthDate;
+
+    private LocalDate createdAt;
+
+    private LocalDate updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDate.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDate.now();
+    }
 }
--- src/main/java/com/julianguerra/financial_app/service/ClientService.java
+++ src/main/java/com/julianguerra/financial_app/service/ClientService.java
@@ -5,6 +5,9 @@ import com.julianguerra.financial_app.exception.BusinessException;
 import com.julianguerra.financial_app.repository.ClientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.time.Period;
+
 /**
  * Servicio encargado de la lógica de negocio para la gestión de clientes.
  * Asegura que cada nuevo registro cumpla con las políticas legales
@@ -28,7 +31,7 @@ public class ClientService {
      */
     public Client createClient(Client client) {
 // --- 1. Validación de Política de Edad ---
-        if (client.isMinor()) {
+        if (Period.between(client.getBirthDate(), LocalDate.now()).getYears() < 18) {
             throw new BusinessException("Client must be over 18 years old");
         }
 // --- 2. Validación de Identidad Única ---
