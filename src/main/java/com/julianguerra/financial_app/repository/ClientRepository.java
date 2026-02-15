7cabf61eefd9bb9aac27a54e7a7c3c70fbdb9271
julianguerra1231186-crypto <julianguerra1231186@gmail.com>
2026-02-15 11:43:53 -0500

    feat : crate ClientRepository interface

---
+++ src/main/java/com/julianguerra/financial_app/repository/ClientRepository.java
@@ -0,0 +1,13 @@
+package com.julianguerra.financial_app.repository;
+
+import com.julianguerra.financial_app.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    Optional<Client> findByIdentificationNumber(String identificationNumber);
+
+    Optional<Client> findByEmail(String email);
+}
